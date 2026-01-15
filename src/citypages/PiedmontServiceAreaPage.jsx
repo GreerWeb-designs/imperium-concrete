c8ef74956a6467d3a858a4cb3c7a6fce2418d9dd
GreerWeb-designs <bob@greerwebdesigns.com>
2026-01-15 09:43:38 -0500

    Update PiedmontServiceAreaPage.jsx

--- src/citypages/PiedmontServiceAreaPage.jsx
+++ src/citypages/PiedmontServiceAreaPage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import imperiumpiedmont from "../assets/ImperiumPiedmont.jpg";
+import imperiumpiedmont from "../assets/Imperiumpiedmont.jpg";
 
 
 export default function PiedmontServiceAreaPage() {
